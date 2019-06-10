62779446e0b9861481c21e794dfc2620aeb2133c
Ivan Solomichev <vanijatko.social@gmail.com>
2019-06-10 13:36:27 +0300

    updated solidity version

--- contracts/Onigiri.sol
+++ contracts/Onigiri.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.8;
 
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
